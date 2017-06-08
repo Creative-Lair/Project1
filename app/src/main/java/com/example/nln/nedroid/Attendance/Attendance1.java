95c4af124cd42aac5d558967971169681570bc62
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-08 05:16:03 +0500

    Attendance Sheet and lecture count done

--- app/src/main/java/com/example/nln/nedroid/Attendance/Attendance1.java
+++ app/src/main/java/com/example/nln/nedroid/Attendance/Attendance1.java
@@ -1,6 +1,5 @@
 package com.example.nln.nedroid.Attendance;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -12,7 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.nln.nedroid.Helper.Student;
-import com.example.nln.nedroid.Nav_AttendOne;
 import com.example.nln.nedroid.R;
 import com.example.nln.nedroid.Session;
 import com.google.firebase.database.ChildEventListener;
@@ -21,7 +19,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +26,7 @@ import java.util.Map;
 
 public class Attendance1 extends AppCompatActivity{
 
+    String[] words;
     private RecyclerView recyclerView;
     private ABAdaptor adapter;
     private List<AButton> albumList;
@@ -36,13 +34,8 @@ public class Attendance1 extends AppCompatActivity{
     private String subject;
     private String section;
     private String timeslot, lectureTopic,lectureType,lectureImp;
-
     private Session session;
-
     private TextView course,time,sec;
-
-    String[] words;
-
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
 
@@ -100,7 +93,7 @@ public class Attendance1 extends AppCompatActivity{
                 ArrayList<String> courses = student.getCourses();
                 for (String c: courses) {
                     if(c.equals(words[0])&&student.getSection().equals(section)){
-                        AButton ab = new AButton(dataSnapshot.getKey().substring(1).toUpperCase()+" "+student.getName(),false);
+                        AButton ab = new AButton(dataSnapshot.getKey().substring(1).toUpperCase() + " " + student.getName(), true);
                         albumList.add(ab);
                         adapter.notifyDataSetChanged();
                     }
@@ -158,9 +151,9 @@ public class Attendance1 extends AppCompatActivity{
 
         databaseRef.child(id).setValue(attendence);
 
+        Toast.makeText(this, "Uploaded", Toast.LENGTH_SHORT).show();
 
-
-
+        finish();
 
     }
 

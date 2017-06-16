39e119820327af2e7ce8d07d2b98ec88f6272c2e
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-16 16:20:53 +0500

    Create Excel Sheet

--- app/src/main/java/com/example/nln/nedroid/Nav_AttendThree_pdf.java
+++ app/src/main/java/com/example/nln/nedroid/Nav_AttendThree_pdf.java
@@ -1,7 +1,12 @@
 package com.example.nln.nedroid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +14,11 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.nln.nedroid.Attendance.DetailAdaptor;
 import com.example.nln.nedroid.Attendance.Lecture;
@@ -20,7 +29,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Nav_AttendThree_pdf extends AppCompatActivity {
 
@@ -35,6 +56,17 @@ public class Nav_AttendThree_pdf extends AppCompatActivity {
     private ArrayList<Lecture_detail> details;
     private DetailAdaptor adapter;
     private ChildEventListener childEventListener;
+    private Button btn;
+
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState);
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(extStorageState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +82,14 @@ public class Nav_AttendThree_pdf extends AppCompatActivity {
             startActivity(i);
             finish();
         }
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         details = new ArrayList<>();
 
@@ -99,6 +139,27 @@ public class Nav_AttendThree_pdf extends AppCompatActivity {
         };
 
         lectureRef.child(code).child(section).addChildEventListener(childEventListener);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (!Settings.System.canWrite(getApplication())) {
+                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE}, 2909);
+                    } else {
+                        // continue with your code
+                        saveFile();
+                    }
+                } else {
+                    // continue with your code
+                    saveFile();
+                }
+
+            }
+        });
 
 
     }
@@ -165,5 +226,77 @@ public class Nav_AttendThree_pdf extends AppCompatActivity {
 
 
         return true;
+    }
+
+    public void saveFile() {
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.e("EXCEL SHEET", "Storage not available or read only");
+            return;
+        }
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet(code + " " + section);
+
+        Map<String, Object[]> data = new HashMap<>();
+        int i = 1;
+        data.put("" + i, new Object[]{"Date", "Timeslot", "Topic"});
+        i++;
+        for (Lecture_detail detail : details) {
+            data.put("" + i, new Object[]{detail.getDate(), detail.getTime(), detail.getTopic()});
+            i++;
+        }
+        Set<String> keyset = data.keySet();
+        int rownum = 0;
+        for (String key : keyset) {
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = data.get(key);
+            int cellnum = 0;
+            for (Object obj : objArr) {
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof Date)
+                    cell.setCellValue((Date) obj);
+                else if (obj instanceof Boolean)
+                    cell.setCellValue((Boolean) obj);
+                else if (obj instanceof String)
+                    cell.setCellValue((String) obj);
+                else if (obj instanceof Double)
+                    cell.setCellValue((Double) obj);
+            }
+        }
+
+        File file = new File(getExternalFilesDir(null), "Lecture_details_" + code + "_" + section + ".xls");
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(file);
+            workbook.write(os);
+            Log.w("FileUtils", "Writing file" + file);
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 2909: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("Permission", "Granted");
+                    saveFile();
+                } else {
+                    Log.e("Permission", "Denied");
+                    Toast.makeText(this, "Can't save the file.. write permission not granted!!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 }
